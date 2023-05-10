32d61a2101c95f1bd1e2e2a1d37fba12cc9a6879
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-10 15:14:25 -0300

    feat: requisito 02
    Crie o modelo User em src/models/User.js com as propriedades corretas

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false, // remove o createdAt e updatedA
+      underscored: true, // padroniza o nome das tabelas e colunas
+      tableName: 'users',
+    }
+  );
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blogPosts',
+    });
+  }
+  return User;
+};
